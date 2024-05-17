dae63d2f2ad36ac6599c663ef3d8d33aadd65420
Robert Albers <robert@robertalbers.se>
2024-05-17 13:44:39 +0200

    update

--- src/app/(authenticated)/page.tsx
+++ src/app/(authenticated)/page.tsx
@@ -3,15 +3,16 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <h1 style={{ fontSize: "32px" }}>Welcome authenticated=true!</h1>
-      <h2 style={{ fontSize: "24px" }}>This is the todo-app</h2>
-      <Link href="/todos">
-        <button
-          type="button"
-          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-        >
+      <h1 className="mt-4" style={{ fontSize: "32px" }}>
+        Welcome authenticated=true!
+      </h1>
+      <h2 className="mt-4" style={{ fontSize: "24px" }}>
+        This is the todo-app
+      </h2>
+      <Link href="/todos" className="mt-4">
+        <div className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
           Get your todos sorted
-        </button>
+        </div>
       </Link>
     </>
   );
--- src/app/(unauthenticated)/layout.tsx
+++ src/app/(unauthenticated)/layout.tsx
@@ -1,4 +1,6 @@
 import type { Metadata } from "next";
+import { redirect } from "next/navigation";
+import { cookies } from "next/headers";
 
 export const metadata: Metadata = {
   title: "Login",
@@ -9,6 +11,7 @@ export default function LoginLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  if (cookies().get("authenticated")) redirect("/");
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
       <div className="bg-gray-500 p-16">{children}</div>
