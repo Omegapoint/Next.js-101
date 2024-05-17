f315dd219028468b87344012edd680252b5d25f3
Robert Albers <robert@robertalbers.se>
2024-05-17 13:55:45 +0200

    Added request to route handler

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,7 +1,7 @@
 import { cookies } from "next/headers";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function POST() {
+export async function POST(req: NextRequest) {
   try {
     cookies().set({ name: "authenticated", value: "true" });
     return NextResponse.json({}, { status: 200 });
